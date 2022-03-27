204d79e0ae47e0716376623982756df0c46bd1ca
Haodong Mo <mohaodong@gmail.com>
2022-03-27 21:15:37 +1100

    MAC-10
    No proper mirror model though

---
+++ lua/weapons/arc9_uli_mac10.lua
@@ -0,0 +1,250 @@
+AddCSLuaFile()
+
+SWEP.Base = "arc9_base"
+
+SWEP.Spawnable = true
+SWEP.Category = "ARC-9 - Ultralight"
+
+SWEP.PrintName = "Ingram MAC-10"
+SWEP.TrueName = "MAC-10"
+
+SWEP.Class = "Handgun"
+SWEP.Trivia = {
+    ["Country of Origin"] = "United States of America",
+    ["Caliber"] = ".45 ACP",
+    ["Weight (Loaded)"] = "3.82kg",
+    ["Projectile Weight"] = "15.2 Grams",
+    ["Muzzle Velocity"] = "919 Feet/Second",
+    ["Muzzle Energy"] = "584 Joules"
+}
+
+SWEP.Credits = {
+    Author = "Arctic",
+    Assets = "Counter-Strike Source"
+}
+
+
+SWEP.Description = [[Essentially a box that bullets come out of, the MAC-10 SMG boasts a high rate of fire, with poor spread accuracy and high recoil as trade-offs.]]
+
+if ARC9:UseTrueNames() then
+
+SWEP.PrintName = SWEP.TrueName
+
+end
+
+SWEP.ViewModel = "models/weapons/cstrike/c_smg_mac10.mdl"
+SWEP.WorldModel = "models/weapons/w_pist_deagle.mdl"
+
+SWEP.Slot = 2
+
+SWEP.MirrorVMWM = true
+SWEP.WorldModelMirror = "models/weapons/cstrike/c_smg_mac10.mdl"
+SWEP.WorldModelOffset = {
+    Pos = Vector(-19, 8, -6),
+    Ang = Angle(-5, 0, 180),
+    Scale = 1
+}
+
+SWEP.SpreadMultHipFire = 3
+SWEP.RecoilMultHipFire = 1.25
+SWEP.RecoilAutoControlMultHipFire = 0.5
+
+-------------------------- DAMAGE PROFILE
+
+SWEP.DamageMax = 24 -- Damage done at point blank range
+SWEP.DamageMin = 15 -- Damage done at maximum range
+
+SWEP.DamageRand = 0.1 -- Damage varies randomly per shot by this fraction. 0.1 = +- 10% damage per shot.
+
+SWEP.RangeMin = 1200 -- How far bullets retain their maximum damage for.
+SWEP.RangeMax = 6000 -- In Hammer units, how far bullets can travel before dealing DamageMin.
+
+SWEP.Penetration = 3 -- Units of wood that can be penetrated by this gun.
+
+-------------------------- PHYS BULLET BALLISTICS
+
+SWEP.PhysBulletMuzzleVelocity = 11000
+
+-------------------------- MAGAZINE
+
+SWEP.Ammo = "pistol" -- What ammo type this gun uses.
+
+SWEP.ChamberSize = 0 -- The amount of rounds this gun can chamber.
+SWEP.ClipSize = 32 -- Self-explanatory.
+SWEP.SupplyLimit = 6 -- Amount of magazines of ammo this gun can take from an ARC9 supply crate.
+SWEP.SecondarySupplyLimit = 2 -- Amount of reserve UBGL magazines you can take.
+
+SWEP.ReloadInSights = false -- This weapon can aim down sights while reloading.
+
+-------------------------- FIREMODES
+
+SWEP.RPM = 850
+
+-- Works different to ArcCW
+
+-- -1: Automatic
+-- 0: Safe. Don't use this for safety.
+-- 1: Semi.
+-- 2: Two-round burst.
+-- 3: Three-round burst.
+-- n: n-round burst.
+SWEP.Firemodes = {
+    {
+        Mode = -1,
+        -- add other attachment modifiers
+    },
+    {
+        Mode = 1
+    }
+}
+-------------------------- RECOIL
+
+-- General recoil multiplier
+SWEP.Recoil = 1
+
+-- These multipliers affect the predictible recoil by making the pattern taller, shorter, wider, or thinner.
+SWEP.RecoilUp = 0.7 -- Multiplier for vertical recoil
+SWEP.RecoilSide = 0.7 -- Multiplier for vertical recoil
+
+-- These values determine how much extra movement is applied to the recoil entirely randomly, like in a circle.
+-- This type of recoil CANNOT be predicted.
+SWEP.RecoilRandomUp = 0.3
+SWEP.RecoilRandomSide = 0.3
+
+SWEP.RecoilDissipationRate = 50 -- How much recoil dissipates per second.
+SWEP.RecoilResetTime = 0.05 -- How long the gun must go before the recoil pattern starts to reset.
+
+SWEP.RecoilAutoControl = 1 -- Multiplier for automatic recoil control.
+
+SWEP.RecoilKick = 1.25
+
+-------------------------- SPREAD
+
+SWEP.Spread = 0.005
+
+SWEP.SpreadAddRecoil = 0.0002 -- Applied per unit of recoil.
+
+-------------------------- HANDLING
+
+SWEP.FreeAimRadius = 12 -- In degrees, how much this gun can free aim in hip fire.
+SWEP.Sway = 2 -- How much the gun sways.
+
+SWEP.SwayMultMidAir = 2
+SWEP.SwayMultMove = 1.15
+SWEP.SwayMultCrouch = 0.66
+SWEP.SwayMultShooting = 1.2
+
+SWEP.FreeAimRadiusSights = 0
+
+SWEP.SwayMultSights = 0.3
+
+SWEP.AimDownSightsTime = 0.25 -- How long it takes to go from hip fire to aiming down sights.
+SWEP.SprintToFireTime = 0.25 -- How long it takes to go from sprinting to being able to fire.
+
+SWEP.SpeedMult = 0.97
+SWEP.SpeedMultSights = 0.75
+SWEP.SpeedMultShooting = 0.7
+SWEP.SpeedMultMelee = 0.75
+SWEP.SpeedMultCrouch = 1
+SWEP.SpeedMultBlindFire = 1
+
+-------------------------- MELEE
+
+SWEP.Bash = true
+SWEP.PrimaryBash = false
+
+SWEP.BashDamage = 50
+SWEP.BashLungeRange = 128
+SWEP.BashRange = 64
+SWEP.PreBashTime = 0.25
+SWEP.PostBashTime = 0.5
+
+-------------------------- TRACERS
+
+SWEP.TracerNum = 1 -- Tracer every X
+SWEP.TracerColor = Color(255, 225, 200) -- Color of tracers. Only works if tracer effect supports it. For physical bullets, this is compressed down to 9-bit color.
+
+-------------------------- POSITIONS
+
+SWEP.IronSights = {
+    Pos = Vector(-10.1, -12, 1.55),
+    Ang = Angle(-5.5, 0.25, -7.25),
+    Midpoint = { -- Where the gun should be at the middle of it's irons
+        Pos = Vector(0, 15, -4),
+        Ang = Angle(0, 0, -45),
+    },
+    Magnification = 1.1,
+}
+
+SWEP.SprintAng = Angle(30, -15, 0)
+SWEP.SprintPos = Vector(5, -8, 4)
+
+SWEP.ActivePos = Vector(-2, -8, 0)
+SWEP.ActiveAng = Angle(0, 0, 0)
+
+SWEP.CrouchPos = Vector(-2, -10, 2)
+SWEP.CrouchAng = Angle(0, 0, 0)
+
+SWEP.CustomizeAng = Angle(90, 0, 0)
+SWEP.CustomizePos = Vector(26, 32, 6)
+SWEP.CustomizeSnapshotFOV = 110
+
+-------------------------- HoldTypes
+
+SWEP.HoldType = "rpg"
+SWEP.HoldTypeSprint = "normal"
+SWEP.HoldTypeHolstered = "normal"
+SWEP.HoldTypeSights = "smg"
+SWEP.HoldTypeCustomize = "slam"
+SWEP.HoldTypeBlindfire = "pistol"
+
+SWEP.AnimShoot = ACT_HL2MP_GESTURE_RANGE_ATTACK_AR2
+SWEP.AnimReload = ACT_HL2MP_GESTURE_RELOAD_AR2
+SWEP.AnimDraw = false
+
+-------------------------- EFFECTS
+
+SWEP.MuzzleParticle = "muzzleflash_smg"
+SWEP.MuzzleEffectQCA = 1
+
+SWEP.ShellModel = "models/shells/shell_9mm.mdl"
+SWEP.ShellCorrectAng = Angle(0, 180, 0)
+SWEP.ShellScale = 1
+SWEP.ShellPhysBox = Vector(0.5, 0.5, 2)
+
+-------------------------- SOUNDS
+
+SWEP.ShootSound = "weapons/mac10/mac10-1.wav"
+SWEP.ShootSoundSilenced = "weapons/tmp/tmp-1.wav"
+SWEP.DryFireSound = "weapons/clipempty_pistol.wav"
+
+SWEP.FiremodeSound = "arc9/firemode.wav"
+
+SWEP.Animations = {
+    ["fire"] = {
+        Source = {"mac10_fire", "mac10_fire2", "mac10_fire3"},
+        -- Source = "mac10_idle",
+        Mult = 0.5
+    },
+    ["reload"] = {
+        Source = "mac10_reload"
+    },
+    ["draw"] = {
+        Source = "mac10_draw"
+    },
+    ["holster"] = {
+        Source = "mac10_draw",
+        Reverse = true
+    },
+    ["idle"] = {
+        Source = "mac10_idle"
+    }
+}
+
+-------------------------- ATTACHMENTS
+
+SWEP.AttachmentElements = {
+}
+
+SWEP.Attachments = {
+}
